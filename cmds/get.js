f7a274691af3596accdde9145a6d2398a0f7b92b
Mike Pirog <mike@kalamuna.com>
2016-08-15 12:19:27 -0700

    Add diff and change to our general information

--- cmds/get.js
+++ cmds/get.js
@@ -7,8 +7,27 @@
 'use strict';
 
 // Get our mmm library
-var mmm = require('./../lib/mmm.js');
 var _ = require('lodash');
+var chalk = require('chalk');
+var mmm = require('./../lib/mmm.js');
+
+/*
+ * Basic helper to colorize up/down data
+ */
+var getColor = function(datum) {
+
+  // Colorize if needed
+  if (datum > 0) {
+    return 'green';
+  }
+  else if (datum < 0) {
+    return 'red';
+  }
+
+  // Guess no change so return original
+  return 'white';
+
+};
 
 // Define the command
 exports.command = 'get [symbol]';
@@ -38,21 +57,46 @@ exports.handler = function(argv) {
   // Translate data into a nice table
   .then(function(data) {
 
+    // Var headers
+    var headers = [
+      'Date',
+      'Open',
+      'High',
+      'Low',
+      'Close',
+      'Volume',
+      'Change',
+      '% Change'
+    ];
+
     // Build the table and headers
     var options = {
-      head: ['Date', 'Open', 'High', 'Low', 'Close', 'Volume'],
+      head: headers,
       style: {head: ['cyan'], border: ['grey']}
     };
 
     // Translate our data into an array of rows
     var rows = _.map(data, function(value) {
+
+      // Update the date
+      value.date = new Date(value.date).toDateString();
+
+      // Colorize the diff
+      value.diff = chalk[getColor(value.diff)](_.round(value.diff, 2));
+
+      // Colorize the change and add some %
+      value.change = _.round(value.change * 100, 2);
+      value.change = chalk[getColor(value.change)](value.change + '%');
+
       return [
-        new Date(value.date).toDateString(),
+        value.date,
         value.open,
         value.high,
         value.low,
         value.close,
-        value.volume
+        value.volume,
+        value.diff ,
+        value.change
       ];
     });
 
